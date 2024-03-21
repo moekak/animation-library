8e41738f1ee07e589532b82d32fa0a810e642701
Moeka <moeka.k0729@gmail.com>
2024-03-21 22:35:22 +0900

    first attempt

---
+++ main.js
@@ -0,0 +1,31 @@
+
+const toggleAnimation = ()=>{
+    document.addEventListener("DOMContentLoaded", ()=>{
+        const target = document.querySelector(".position_bar")
+        const btn = document.querySelector(".btn")
+        
+        // ターゲットの位置
+        const distanceFromTop = target.offsetTop;
+
+
+        window.addEventListener("scroll", ()=>{
+            const currentPosition = window.scrollY
+            // ドキュメント全体の高さ
+            const documentHeight = document.documentElement.scrollHeight;
+            // ビューポートの高さ
+            const windowHeight = window.innerHeight;
+
+            if(distanceFromTop <= currentPosition){
+                btn.classList.add("active")
+            }
+
+
+            if(distanceFromTop >= currentPosition || currentPosition + windowHeight >= documentHeight){
+                btn.classList.remove("active")
+            }
+        })
+    })
+}
+
+
+module.exports = toggleAnimation()
